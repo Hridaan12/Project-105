b31870dd29ed47ee9f07ac75cb262a2f9166ad51
Hridaan12 <71926511+Hridaan12@users.noreply.github.com>
2021-02-15 23:48:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_qwallty:90
+    });
+    function take_snapshot(){
+        Webcam.snap(function(data_uri){ 
+        });
+    }
+        function Hridaan(){
+            snap()
+        }
+        function Mother(){
+            snap()
+        }
+        function Father(){
+            snap()
+        }
+    
+    function snap(){
+        webcam.snap
+    }
